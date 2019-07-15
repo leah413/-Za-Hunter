0ab690468de35714cca7bfe3d8d7fa2f36e7525b
Leah Nella <leahnella@yahoo.com>
2019-07-15 14:14:01 -0500

    Collected and passed data in segue

--- 'Za Hunter/LocationsDetailsviewController.swift
+++ 'Za Hunter/LocationsDetailsviewController.swift
@@ -7,24 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class LocationsDetailsviewController: UIViewController {
 
+    var selectedMapItem = MKMapItem()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print(selectedMapItem.name!)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
